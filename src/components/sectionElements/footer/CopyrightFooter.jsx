104dac7f517ad93414d37ad857084a59bc52c4d9
Edison Matos <edison.matos@live.com>
2026-06-06 08:26:06 -0300

    feat: Correções solicitadas

--- src/components/sectionElements/footer/CopyrightFooter.jsx
+++ src/components/sectionElements/footer/CopyrightFooter.jsx
@@ -6,8 +6,9 @@ function CopyrightFooter() {
     <div className="text-center w-full phone2:w-[90%] max-w-[1110px] flex flex-col mb-[48px] phone2:mb-[80px] tablet1:mb-[27px]">
       <p>
         Razão Social: Ingrid Candido Vargas Rodrigues Sociedade Individual de
+        Advocacia
       </p>
-      <p className="mb-4">Advocacia CNPJ: 54.994.667/0001-86</p>
+      <p className="mb-4">CNPJ: 54.994.667/0001-86</p>
       <p className="w-full opacity-75">{content.texts.footer.copyrightLine}</p>
       <p>
         <a
